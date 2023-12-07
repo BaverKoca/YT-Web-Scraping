74491bd47c3bb98d090af683f1b7cf67408e26f6
Baver Koca <142082051+BaverKoca@users.noreply.github.com>
2023-12-07 10:21:57 +0200

    Update YT-LiveChat-Draw.py

--- YT-LiveChat-Draw.py
+++ YT-LiveChat-Draw.py
@@ -7,12 +7,12 @@ from bs4 import BeautifulSoup
 import time
 import random
 
-browser_data = webdriver.Chrome() # You can use any kind of search motor you want. Just check is it give permission to scraping.
+browser_data = webdriver.SearchEngine() # You can use any kind of search engine you want. Just check it is give permission to scraping.
 eligibleUsers = set()
 
 def getHTML(url, browser_instance):
     browser_instance.get(url)
-    time.sleep(5) # You  can change time for wait there. But if chat is so crawded or your internet speed is slow, you can change till 30 second
+    time.sleep(5) # You  can change time for wait there. But if chat is so crowded or your internet speed is slow, you can change till 30 second.
     return browser_instance.page_source
 
 def parselHTML(html_source):
@@ -40,14 +40,16 @@ eligibleUsers = getEligibleUsers(messages)
 print("Eligible users are: ", eligibleUsers)
 print("Total eligible users:", len(eligibleUsers)) # Here you can see how many people wrote your keywords in chat.
 time.sleep(2) # You  can change time for wait there.
-browser_data.quit()
+browser_data.quit() # Quit from search engine.
 time.sleep(3) # You  can change time for wait there.
 
-for i in range(1, 4): # It's optional. You don't have to use that loop if you wanna see the winner quick.
+# This for loop is optional where's upder here. You don't have to use that loop if you wanna see the winner quick. You can delete or put in comment line until here to...
+for i in range(1, 4):
     dots = i * "."
     print("A random number is draw." + dots) # Here waiting till show to winner from eligible users.
     time.sleep(1) # That is waiting time for between of all "A random number is draw." loop.
 print("\n\n")
+# ... here.
 
 eligibleUsersList = list(eligibleUsers)
 print("And the winner...", random.choice(eligibleUsersList)) # That code is for show the winner.
